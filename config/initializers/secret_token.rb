d499f293dfedb43a1e3888c2d6f51e05ab6652ae
Bastien Beurier <bastienbeurier@gmail.com>
2013-04-06 12:49:28 -0700

    second commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+StreetShoutWorker1::Application.config.secret_token = '2657dcbad26909ca6f59ce07bd044d2868537d55cca5d8d12e464491aec873559de4a1a63705a7d5bd5a9e10ca4e6b035a3b3bd4357e38e05bcfd76febdacc84'
