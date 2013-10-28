dab1c56fb49d777b4b2bac8a8a68a59d05f516a1
Bastien Beurier <bastienbeurier@gmail.com>
2013-10-29 00:58:01 +0100

    Debug &&

--- app/models/shout.rb
+++ app/models/shout.rb
@@ -1,10 +1,4 @@
 class Shout < ActiveRecord::Base
-  acts_as_mappable  :default_units => :kms, 
-                    :default_formula => :sphere, 
-                    :distance_field_name => :distance,
-                    :lat_column_name => :lat,
-                    :lng_column_name => :lng
-
   attr_accessible   :description, :lat, :lng, :source, :created_at, :display_name, :device_id, :image
 
   validates :description, presence: true, length: { maximum: 140 }
