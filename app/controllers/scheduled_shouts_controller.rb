5876d737ec0d3b4995361b596208a371d410bcb7
Bastien Beurier <bastienbeurier@gmail.com>
2013-10-29 02:19:54 +0100

    Debug 16

--- app/controllers/scheduled_shouts_controller.rb
+++ app/controllers/scheduled_shouts_controller.rb
@@ -11,7 +11,7 @@ class ScheduledShoutsController < ApplicationController
 
   # POST /scheduled_shouts
   def create
-    if params[:scheduled_shout][:password] = "shoutouillons"
+    if params[:scheduled_shout][:password] == "shoutouillons"
         shout_date_time = DateTime.new(params[:scheduled_shout]["scheduled_time(1i)"].to_i, 
                                     params[:scheduled_shout]["scheduled_time(2i)"].to_i,
                                     params[:scheduled_shout]["scheduled_time(3i)"].to_i,
@@ -24,13 +24,13 @@ class ScheduledShoutsController < ApplicationController
 
         respond_to do |format|
           if @scheduled_shout.save
-            format.html { render action: "new", notice: 'You rock!' }
+            format.html { render root_path, notice: 'You rock!' }
           else
-            format.html { render action: "new", notice: 'Bad luck, it failed...' }
+            format.html { render root_path, notice: 'Bad luck, it failed...' }
           end
         end
     else 
-        format.html { render action: "new", notice: 'Wrong password dude, get outta here!!!' }    
+        format.html { render root_path, notice: 'Wrong password dude, get outta here!!!' }    
     end 
   end
 end
