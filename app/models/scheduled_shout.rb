07936cc1f608a7ca3428963723afbf9ead64b229
Bastien Beurier <bastienbeurier@gmail.com>
2013-10-29 01:58:20 +0100

    Debug

--- app/models/scheduled_shout.rb
+++ app/models/scheduled_shout.rb
@@ -1,5 +1,5 @@
 class ScheduledShout < ActiveRecord::Base
-  attr_accessible   :description, :lat, :lng, :scheduled_time, :display_name, :image, :author
+  attr_accessible   :description, :lat, :lng, :scheduled_time, :display_name, :image, :author, :password
   
   validates :description, presence: true, length: { maximum: 140 }
   validates :display_name, presence: true, length: { maximum: 20 }
