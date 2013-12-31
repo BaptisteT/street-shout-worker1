7aa4925609dfe07725bde9fc0c189cbc9c4fd60e
Baptiste <baptiste.truchot@gmail.com>
2013-12-31 17:33:53 +0100

    Constant (12)

--- app/models/scheduled_shout.rb
+++ app/models/scheduled_shout.rb
@@ -10,12 +10,12 @@ class ScheduledShout < ActiveRecord::Base
                           {:greater_than => Proc.new{|r| Time.now}, :message => "the date should be in the future"}
   validates :author, presence: true
 
- # with_options :if => :is_born do |shout|
- #   shout.validates :lat, :numericality => { :greater_than => BORN_LAT_MIN, :less_than_or_equal_to => BORN_LAT_MAX,
- #                                                                             :message => "This is not in the 3 Vallees"} 
- #   shout.validates :long, :numericality => { :greater_than => BORN_LONG_MIN, :less_than_or_equal_to => BORN_LONG_MAX,
- #                                                                             :message => "This is not in the 3 Vallees"}  
- # end
+ with_options :if => :is_born do |shout|
+   shout.validates :lat, :numericality => { :greater_than => BORN_LAT_MIN, :less_than_or_equal_to => BORN_LAT_MAX,
+                                                                             :message => "This is not in the 3 Vallees"} 
+   shout.validates :long, :numericality => { :greater_than => BORN_LONG_MIN, :less_than_or_equal_to => BORN_LONG_MAX,
+                                                                             :message => "This is not in the 3 Vallees"}  
+ end
 
   Paperclip.interpolates :file_name do |attachment, style|
     attachment.instance.id.to_s + "--400"
