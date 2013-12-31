b055676eb6a76473a4e094016abffe25d4a94c4e
Baptiste <baptiste.truchot@gmail.com>
2013-12-31 18:34:59 +0100

    Constant (23)

--- app/models/scheduled_shout.rb
+++ app/models/scheduled_shout.rb
@@ -6,7 +6,7 @@ class ScheduledShout < ActiveRecord::Base
   validates :display_name, presence: true, length: { maximum: 20 }
   validates :lat,         presence: true
   validates :lng,         presence: true
-  validates :is_valid_time?
+  validate :is_valid_time?
   validates :author, presence: true
 
  with_options :if => :is_born do |shout|
@@ -29,11 +29,11 @@ class ScheduledShout < ActiveRecord::Base
   private
 
   def is_valid_time?
-    # if ! scheduled_time.is_a?(Date)
-    #   errors.add(:scheduled_time,'must be a valid date')
-    # end
-    # if :scheduled_time < Time.now
-    #   errors.add(:scheduled_time,'must be in the future')
-    # end
+    if ! scheduled_time.is_a?(Date)
+      errors.add(:scheduled_time,'must be a valid date')
+    end
+    if scheduled_time < Time.now
+      errors.add(:scheduled_time,'must be in the future')
+    end
   end
 end
